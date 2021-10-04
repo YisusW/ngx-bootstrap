073b57ef78061a9be40a4a677b3d207a20ae49c3
YisusW <jesusochoa266@gmail.com>
2021-10-03 22:33:56 -0500

    test(datepicker-inner): add test in oninit method

--- src/datepicker/testing/datepicker-inner.component.spec.ts
+++ src/datepicker/testing/datepicker-inner.component.spec.ts
@@ -23,4 +23,32 @@ describe('datepicker-inner:', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onInit method', () => {
+    it('case initDate is valid', () => {
+      component.initDate = new Date();
+
+      component.ngOnInit();
+
+      expect(component.activeDate).toBe(component.initDate);
+    });
+
+    it('should emit a new update activeDate', () => {
+      component.initDate = new Date('2021-10-03');
+      const updateEmit = jest.spyOn(component.update, 'emit');
+
+      component.ngOnInit();
+
+      expect(updateEmit).toHaveBeenCalledWith(component.activeDate);
+    });
+
+    it('case activateDate is undefined', () => {
+      component.initDate = undefined;
+
+      component.ngOnInit();
+
+      expect(component.activeDate).not.toBe(component.initDate);
+    });
+
+  });
 });
